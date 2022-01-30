8f4d380e03a2abf4704d68b3869ac2aefc7a6c20
jiahao-0204 <75384852+jiahao-0204@users.noreply.github.com>
2022-01-30 15:33:10 +0000

    added question description

--- B16 Software Desgin 1/Q1.cpp
+++ B16 Software Desgin 1/Q1.cpp
@@ -1,4 +1,18 @@
-// hello world
+/**
+ * * Hello world!
+ * 
+ * The goal of this question is for you to edit, compile, and execute a simple C++ program. 
+ * This is to ensure you have access to the necessary programming tools and are able to use 
+ * them. You can then use the same tools later for more complex questions.
+ * 
+ * (a) Set up your programming environment. Refer to the section “A First C++ Programme” in the 
+ * lecture notes for some suggestions for how to do this. In B16 we recommend the Visual Studio 
+ * Code IDE, since this is widely used and runs on all common operating systems; or a text editor 
+ * plus command line compiler. If you encounter problems, Stack Overflow is a good source of 
+ * answers (as is Google).
+ * 
+ * (b) Write, compile, and execute a C++ programme which prints “Hello world!” to the console.
+ */
 
 #include <iostream>
 #include <string>
