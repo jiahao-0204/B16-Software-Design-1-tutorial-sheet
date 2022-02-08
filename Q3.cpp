32b8478c2432503966bcd5879d5245543b5369af
jiahao-0204 <75384852+jiahao-0204@users.noreply.github.com>
2022-02-08 12:18:12 +0000

    added answer to compile-time polymorphism

--- Q3.cpp
+++ Q3.cpp
@@ -7,7 +7,8 @@
  * is the hiding of the implementation of the function from its interface. 
  * 
  * ? (b) compile-time polymorphism
- * todo answer this when finished software design 2
+ * when using 'template' or 'auto' keywords, the compiler will generate the required code automatically upon compilation
+ * this reduce code redundancy and improve portability.
  * 
  * ? (c) run-time polymorphism
  * if a member function is declared virtual, upon execution of this function, the compiler will check any children class has 
